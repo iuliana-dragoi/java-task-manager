a8ed868724f7a0ebfcb1f03e29e6a406dc44ed5f
iuliana.dragoi <iulianadragoi95@gmail.com>
2026-01-21 14:53:33 +0200

    Use @Nested

--- src/test/java/com/example/taskManager/TaskManagerTest.java
+++ src/test/java/com/example/taskManager/TaskManagerTest.java
@@ -1,9 +1,7 @@
 package com.example.taskManager;
 
 import com.example.taskManager.model.Task;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -23,54 +21,69 @@ public class TaskManagerTest {
         assertEquals(0, taskManager.count());
     }
 
-    @Test
-    public void addTask() {
-        taskManager.add(task);
-        assertTrue(taskManager.exists(task.getId()));
-        assertEquals(1, taskManager.count());
+    @Nested
+    @DisplayName("Add Task Tests")
+    class AddTaskTests {
+
+        @Test
+        public void addTask() {
+            taskManager.add(task);
+            assertTrue(taskManager.exists(task.getId()));
+            assertEquals(1, taskManager.count());
+        }
+
+        @Test
+        public void addTask_duplicateTasks() {
+            Task task1 = new Task("Test 2");
+            taskManager.add(task);
+            taskManager.add(task1);
+            assertTrue(taskManager.exists(task.getId()));
+            assertTrue(taskManager.exists(task1.getId()));
+            assertEquals(2, taskManager.count());
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "Task 1, Task 1",
+                "Task 2, Task 2",
+                "Task 3, Task 3"
+        })
+        public void addTask_withCvsSource(String description, String expectedDescription) {
+            Task task = new Task(description);
+            taskManager.add(task);
+
+            Task retrieved = taskManager.getTask(task.getId());
+            assertEquals(expectedDescription, retrieved.getDescription());
+            assertEquals(task.getId(), retrieved.getId());
+        }
     }
 
-    @Test
-    public void addTask_duplicateTasks() {
-        Task task1 = new Task("Test 2");
-        taskManager.add(task);
-        taskManager.add(task1);
-        assertTrue(taskManager.exists(task.getId()));
-        assertTrue(taskManager.exists(task1.getId()));
-        assertEquals(2, taskManager.count());
+    @Nested
+    @DisplayName("Get Task Tests")
+    class GetTaskTests {
+
+        @Test
+        public void getTask() {
+            taskManager.add(task);
+            Task task1 = taskManager.getTask(task.getId());
+            assertEquals("TEST", task1.getDescription());
+            assertTrue(task1.getId() > 0);
+            assertEquals(task, task1);
+        }
     }
 
-    @ParameterizedTest
-    @CsvSource({
-        "Task 1, Task 1",
-        "Task 2, Task 2",
-        "Task 3, Task 3"
-    })
-    public void addTask_withCvsSource(String description, String expectedDescription) {
-        Task task = new Task(description);
-        taskManager.add(task);
+    @Nested
+    @DisplayName("Remove Task Tests")
+    class RemoveTaskTest {
 
-        Task retrieved = taskManager.getTask(task.getId());
-        assertEquals(expectedDescription, retrieved.getDescription());
-        assertEquals(task.getId(), retrieved.getId());
-    }
-
-    @Test
-    public void removeTask() {
-        taskManager.add(task);
-        int preTaskCount = taskManager.count();
-        taskManager.remove(task.getId());
-        assertFalse(taskManager.exists(task.getId()));
+        @Test
+        public void removeTask() {
+            taskManager.add(task);
+            int preTaskCount = taskManager.count();
+            taskManager.remove(task.getId());
+            assertFalse(taskManager.exists(task.getId()));
 //        assertEquals(preTaskCount - 1, taskManager.count());
-    }
-
-    @Test
-    public void getTask() {
-        taskManager.add(task);
-        Task task1 = taskManager.getTask(task.getId());
-        assertEquals("TEST", task1.getDescription());
-        assertTrue(task1.getId() > 0);
-        assertEquals(task, task1);
+        }
     }
 
     @AfterEach
