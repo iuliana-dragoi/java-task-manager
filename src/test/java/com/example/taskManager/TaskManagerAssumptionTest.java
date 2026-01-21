3ed9d2c692837a6ec1c56cef7f87bab15492f1b0
iuliana.dragoi <iulianadragoi95@gmail.com>
2026-01-21 15:20:55 +0200

    Use Assumption Api

---
+++ src/test/java/com/example/taskManager/TaskManagerAssumptionTest.java
@@ -0,0 +1,38 @@
+package com.example.taskManager;
+import com.example.taskManager.model.Task;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+public class TaskManagerAssumptionTest {
+
+    @Test
+    public void testOnlyOnJava21() {
+        assumeTrue(System.getProperty("java.version").startsWith("17"),
+                "Test skipped: Only for Java 21");
+        // ...
+    }
+
+    @Test
+    void testOnlyOnDevEnvironment() {
+        assumeTrue("development".equals(System.getenv("ENVIRONMENT")),
+                "Skipped: Not in development environment");
+
+        //...
+    }
+
+    @Test
+    void testWithConditionalLogic() {
+        TaskManager manager = new TaskManager();
+
+        assumingThat(System.getProperty("os.name").startsWith("Linux"), () -> {
+            manager.add(new Task("Linux task"));
+            assertEquals(1, manager.count());
+        });
+
+        manager.add(new Task("General task"));
+        assertEquals(1, manager.count());
+    }
+}
